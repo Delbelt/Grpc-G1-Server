a909d34d17a72ad4b4e38461cdb92c2beb393824
Nahuel Flores <nahuelhernanflores25@gmail.com>
2024-09-12 14:20:55 -0300

    FEAT: add exceptions handlers

--- src/main/java/server/grpc/UserGrpcService.java
+++ src/main/java/server/grpc/UserGrpcService.java
@@ -1,7 +1,10 @@
 package server.grpc;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import server.UserGrpcServiceGrpc.UserGrpcServiceImplBase;
@@ -11,23 +14,50 @@ import server.services.IUserService;
 
 @GrpcService
 public class UserGrpcService extends UserGrpcServiceImplBase {
-	
+
 	@Autowired
 	private IUserService services;
-	
+
 	@Override
 	public void getUserGrpc(RequestId request, StreamObserver<UserGrpc> responseObserver) {
 
-		var response = services.findById(request.getId());
-		
-		UserGrpc user = 
-				UserGrpc
-				.newBuilder()
-				.setId(response.getId())
-				.setName(response.getName())
-				.build();
+		try {
 
-		responseObserver.onNext(user);
-		responseObserver.onCompleted();
+			var response = services.findById(request.getId());
+
+			if (response == null) {
+				
+				String messageError = "User with id " + request.getId() + " not found";
+				
+				throw new NoSuchElementException(messageError);
+			}
+
+			UserGrpc user = 
+					UserGrpc
+					.newBuilder()
+					.setId(response.getId())
+					.setName(response.getName())
+					.build();
+
+			responseObserver.onNext(user);
+			responseObserver.onCompleted();
+
+		}
+
+		catch (NoSuchElementException e) {			
+			
+			responseObserver
+			.onError(Status.NOT_FOUND.withDescription(e.getMessage())
+			.asRuntimeException());
+		}
+
+		catch (Exception e) {
+			
+			String messageError = "Internal server error: " + e.getMessage();
+			
+			responseObserver
+			.onError(Status.INTERNAL.withDescription(messageError)
+			.asRuntimeException());
+		}
 	}
 }

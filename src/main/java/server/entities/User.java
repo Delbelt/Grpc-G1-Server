8a0ee343f00cdaf50bc4b4db083d405b51e59717
Nahuel Flores <nahuelhernanflores25@gmail.com>
2024-09-22 15:08:15 -0300

    REF: add properties to User

--- src/main/java/server/entities/User.java
+++ src/main/java/server/entities/User.java
@@ -1,25 +1,43 @@
 package server.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-// INITIAL COMMIT: PERSISTENCIA DE PRUEBA - SE BORRARA EN PROXIMAS REVISIONES.
-
-@Data // boilerplate
-@Entity // Para que se considere entidad
-@Table(name="user") // nombre que tendra en la base de datos
+@Data
+@Entity
+@Table(name="user")
 public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@Column(name="username", nullable=false)
+	private String userName;
+	
+	@Column(name="password", nullable=false)
+	private String password;
+	
 	@Column(name="name", nullable=false)
 	private String name;
+	
+	@Column(name="surname", nullable=false)
+	private String surname;
+	
+	@ManyToOne(cascade=CascadeType.PERSIST) 
+	@JoinColumn(name="idRole")
+	private UserRole role;
+	
+	@Column(name="active", nullable=false)
+	private boolean active;
+	
 
 }

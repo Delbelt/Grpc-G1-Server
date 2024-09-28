416ad24e5a9514a0b357506b72da34d8cb6cd0ef
ErikaGeraldine <geraldinevaldez120@gmail.com>
2024-09-28 00:49:02 -0300

    feat: method findall

--- src/main/java/server/repositories/IProductRepository.java
+++ src/main/java/server/repositories/IProductRepository.java
@@ -9,7 +9,7 @@ import server.entities.Product;
 public interface IProductRepository extends JpaRepository<Product, String> {
 	public Product findByCode(String code);
 	public Product findByName(String name);
-	//public List<Product> findAll();
+	public List<Product> findAll(); // este metodo rompio todo arreglar el mvn
 	//public boolean insertOrUpdate(Product product);
 	//public boolean deleteByCode(String code); 
 }

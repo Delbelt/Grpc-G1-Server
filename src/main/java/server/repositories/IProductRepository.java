003976f62a22ef1f2985bcb3826922c49a2f3cd6
ErikaGeraldine <geraldinevaldez120@gmail.com>
2024-10-03 11:13:59 -0300

    feat: add findProductsByFilter

--- src/main/java/server/repositories/IProductRepository.java
+++ src/main/java/server/repositories/IProductRepository.java
@@ -3,16 +3,33 @@ package server.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import server.entities.Product;
 
 public interface IProductRepository extends JpaRepository<Product, String> {
 	public Product findByCode(String code);
 	public Product findByName(String name);
-	public List<Product> findAll(); // este metodo rompio todo arreglar el mvn
+	public List<Product> findAll(); 
+	
+	@Query("SELECT p FROM Product p WHERE "
+	         + "(:code IS NULL OR p.code = :code) AND "
+	         + "(:name IS NULL OR p.name = :name) AND "
+	         + "(:size IS NULL OR p.size = :size) AND "
+	         + "(:color IS NULL OR p.color = :color)")
+	    List<Product> findProductsByFilter(
+	        @Param("code") String code, 
+	        @Param("name") String name, 
+	        @Param("size") String size, 
+	        @Param("color") String color
+	    );
+	}
+	//public List<Product> findProductsByFilter(String code, String name, String size, String color);
+
 	//public boolean insertOrUpdate(Product product);
 	//public boolean deleteByCode(String code); 
-}
+
 
 
 

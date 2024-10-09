103343c6f3df09508df6713cc38a855605ad22b6
Nahuel Flores <nahuelhernanflores25@gmail.com>
2024-10-09 20:40:59 -0300

    FEAT: add utils for kafka

---
+++ src/main/java/server/util/StatePurchaseOrder.java
@@ -0,0 +1,12 @@
+package server.util;
+
+public class StatePurchaseOrder {
+	
+	public final static String REQUESTED = "SOLICITADA";
+	public final static String REJECTED = "RECHAZADA";
+	public final static String ACCEPTED = "ACEPTADA";
+	public final static String PAUSED = "PAUSADA";
+	public final static String RECEIVED = "RECIBIDA";
+	public final static String PROVIDER = "provider111";
+
+}
---
+++ src/main/java/server/util/TopicsKafka.java
@@ -0,0 +1,13 @@
+package server.util;
+
+public class TopicsKafka {
+
+	public final static String PURCHASE_ORDER = "orden-de-compra";
+	public final static String REQUESTED = "-solicitudes";
+	public final static String REQUESTS = "solicitudes";
+	public final static String NEWS = "novedades";
+	public final static String RECEPTION = "recepcion";
+	public final static String DISPATCH = "despacho";
+	
+	public final static String groupIdTopics = "grpc-consumer";
+}
